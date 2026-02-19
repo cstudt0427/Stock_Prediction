12242cdbdff02afeb79921ffae2d72eaeb3e5497
cstudt0427 <c.studt@tcu.edu>
2026-02-19 12:45:06 -0600

    remove predictor import

--- Portfolio/StreamlitApp_Week_3.py
+++ Portfolio/StreamlitApp_Week_3.py
@@ -11,7 +11,6 @@ import tempfile
 
 import boto3
 import sagemaker
-from sagemaker.base_predictor import Predictor
 from sagemaker.serializers import CSVSerializer
 from sagemaker.deserializers import JSONDeserializer
 from sagemaker.serializers import NumpySerializer
@@ -151,6 +150,7 @@ if submitted:
         display_explanation(input_df,session, aws_bucket)
     else:
         st.error(res)
+
 
 
 
