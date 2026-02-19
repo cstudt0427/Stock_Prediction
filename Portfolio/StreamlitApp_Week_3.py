442e8efd609e06473558ded15b9b6dd83e1e8baf
cstudt0427 <c.studt@tcu.edu>
2026-02-19 13:38:06 -0600

    change deployment
    Updated prediction input handling and streamlined the prediction process.

--- Portfolio/StreamlitApp_Week_3.py
+++ Portfolio/StreamlitApp_Week_3.py
@@ -140,17 +140,21 @@ with st.form("pred_form"):
 
 if submitted:
 
-    data_row = [user_inputs[k] for k in MODEL_INFO["keys"]]
-    # Prepare data
-    base_df = df_features
-    input_df = pd.concat([base_df, pd.DataFrame([data_row], columns=base_df.columns)])
-    
-    res, status = call_model_api(input_df)
+    # Build one-row dataframe with correct feature names
+    input_row_df = pd.DataFrame(
+        [[user_inputs[k] for k in MODEL_INFO["keys"]]],
+        columns=MODEL_INFO["keys"]
+    )
+
+    # Call SageMaker endpoint
+    res, status = call_model_api(input_row_df)
+
     if status == 200:
         st.metric("Prediction Result", res)
-        display_explanation(input_df,session, aws_bucket)
     else:
         st.error(res)
+
+
 
 
 
